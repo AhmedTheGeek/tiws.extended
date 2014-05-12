c62b3a96452bfc146681997c4cc84a8b9314a360
Ahmed Hussein <valodes@gmail.com>
2014-05-12 15:44:13 +0200

    _enableReconnecting & _checkMessagingStatusInterval
    Adding _enableReconnecting and MessageStatus Checking Interval

--- socketHandler.js
+++ socketHandler.js
@@ -7,15 +7,17 @@ var _connectionStatus = false;
 var _lastReceivedMessage = 0;
 var _isReconnecting = false;
 var _argu = null;
+var _enableReconnecting = false;
+var _checkMessagingStatusInterval = 10;
 
 //Module Constructor
 var socketHandler = function(args) {
-	
+
 	//set params
 	argu = args;
 	//create object refrence
 	var that = this;
-	
+
 	//Setup the timer checker timer, and add object refrence to the timer object ot be able to access the socketHandler from inside the EventListener
 	setTimeout(function() {
 		that.timer.start({
@@ -24,71 +26,74 @@ var socketHandler = function(args) {
 		});
 
 	}, 5000);
-	
+
 	//Set object refrences to be accessable inside events handlers
 	this.timer.object = this;
 	this.registerTimer(this);
 	websocket.object = this;
-	
+
 	//Listen for disconnection
 	Ti.App.addEventListener('disconnected', function(event) {
 
-		Ti.App.fireEvent("reconnecting", {
-			object : websocket
-		});
-		
-		//Attemp reconnection on 2 steps seperated with 3 seconds, to give time to null the object and create the one
-		if (!_isReconnecting) {
-			
-			//null the object and create new one
-			websocket = null;
-			websocket = io.createWS();
-			_isReconnecting = true;
+		if (_enableReconnecting) {
 
-		} else {
-			
-			//attemp opening the connection and fire the open event
-			websocket.open(argu["URL"]);
-			websocket.addEventListener('open', function(ev) {
-				_isReconnecting = false;
-
-				Ti.App.fireEvent("connected", {
-					object : this.object
-				});
-
+			Ti.App.fireEvent("reconnecting", {
+				object : websocket
 			});
 
-			websocket.addEventListener('message', function(ev) {
-				// ev.data contains message data
-				
-				Ti.App.fireEvent("socketMessage", {
-					event: ev
+			//Attemp reconnection on 2 steps seperated with 3 seconds, to give time to null the object and create the one
+			if (!_isReconnecting) {
+
+				//null the object and create new one
+				websocket = null;
+				websocket = io.createWS();
+				_isReconnecting = true;
+
+			} else {
+
+				//attemp opening the connection and fire the open event
+				websocket.open(argu["URL"]);
+				websocket.addEventListener('open', function(ev) {
+					_isReconnecting = false;
+
+					Ti.App.fireEvent("connected", {
+						object : this.object
+					});
+
 				});
 
-			});
-			
-			//Listen for closing
-			websocket.addEventListener('close', function(ev) {
+				websocket.addEventListener('message', function(ev) {
+					// ev.data contains message data
 
-				Ti.App.fireEvent("disconnected", {
-					object : this.object
+					Ti.App.fireEvent("socketMessage", {
+						event : ev
+					});
+
 				});
 
-				socketHandler.connected = false;
-			});
-			
-			//Listen for Errors
-			websocket.addEventListener('error', function(ev) {
-				// ev.error contains error description, if any
+				//Listen for closing
+				websocket.addEventListener('close', function(ev) {
 
-				Ti.App.fireEvent("disconnected", {
-					object : this.object
+					Ti.App.fireEvent("disconnected", {
+						object : this.object
+					});
+
+					socketHandler.connected = false;
 				});
-			});
 
+				//Listen for Errors
+				websocket.addEventListener('error', function(ev) {
+					// ev.error contains error description, if any
+
+					Ti.App.fireEvent("disconnected", {
+						object : this.object
+					});
+				});
+
+			}
 		}
 	});
-	
+
 	//Listen for connection
 	Ti.App.addEventListener("connected", function(event) {
 		_connectionStatus = true;
@@ -96,18 +101,16 @@ var socketHandler = function(args) {
 
 };
 
-
 //Require timely module
 socketHandler.prototype.timer = require("ti.mely").createTimer();
 
-//Checks the messaging status every few seconds 
+//Checks the messaging status every few seconds
 socketHandler.prototype.checkMessagingStatus = function(event) {
 
 	currentTime = Math.round((new Date()).getTime() / 1000);
 
-	if ((currentTime - _lastReceivedMessage) >= 10 || _connectionStatus == false) {
+	if ((currentTime - _lastReceivedMessage) >= _checkMessagingStatusInterval || _connectionStatus == false) {
 		this.object.fireDisconnected(null);
-
 	}
 
 };
@@ -118,57 +121,54 @@ socketHandler.prototype.registerTimer = function() {
 };
 
 //do the initial connection
-socketHandler.prototype.connect = function(uri) {
+socketHandler.prototype.connect = function() {
 	websocket.open(argu["URL"]);
 	Ti.App.fireEvent("opening", {
-		time : new Date().getTime()
+	time : new Date()).getTime() / 1000
+});
+
+websocket.addEventListener('open', function(ev) {
+	_isReconnecting = false;
+
+	Ti.App.fireEvent("connected", {
+		object : this.object
 	});
 
-	websocket.addEventListener('open', function(ev) {
-		_isReconnecting = false;
+});
 
-		Ti.App.fireEvent("connected", {
-			object : this.object
-		});
+websocket.addEventListener('message', function(ev) {
+	// ev.data contains message data
+	_lastReceivedMessage = Math.round((new Date()).getTime() / 1000);
 
+	Ti.App.fireEvent("socketMessage", {
+		event : ev
 	});
 
-	websocket.addEventListener('message', function(ev) {
-		// ev.data contains message data
-		_lastReceivedMessage = Math.round((new Date()).getTime() / 1000);
-		var stocks = eval("(" + ev.data + ")");
+});
 
-		for (var i = 0; i < stocks.length; i++) {
-			cellName = stocks[i].stockName;
-			cell = Grid.cellIndex[cellName];
-			Grid.updateCell(cell, stocks[i].stockName, stocks[i].changeValue, stocks[i].lastPrice, stocks[i].change, stocks[i].stockCurrency);
-		}
+websocket.addEventListener('close', function(ev) {
 
+	Ti.App.fireEvent("disconnected", {
+		object : this.object
 	});
 
-	websocket.addEventListener('close', function(ev) {
+	socketHandler.connected = false;
+});
 
-		Ti.App.fireEvent("disconnected", {
-			object : this.object
-		});
+websocket.addEventListener('error', function(ev) {
+	// ev.error contains error description, if any
 
-		socketHandler.connected = false;
+	Ti.App.fireEvent("disconnected", {
+		object : this.object
 	});
-
-	websocket.addEventListener('error', function(ev) {
-		// ev.error contains error description, if any
-
-		Ti.App.fireEvent("disconnected", {
-			object : this.object
-		});
-	});
+});
 };
 
 //Fires disconnected event
 socketHandler.prototype.fireDisconnected = function() {
 	Ti.App.fireEvent("disconnected", {
-		time : new Date().getTime(),
-		object : this
+	time : new Date()).getTime() / 1000,
+	object : this
 	});
 	_connectionStatus = false;
 };
@@ -182,5 +182,28 @@ socketHandler.prototype.sendMessage = function(message) {
 	}
 };
 
+socketHandler.prototype.close = function() {
+	websocket.close();
+};
+
+socketHandler.prototype.setEnableReconnecting = function(value) {
+	_enableReconnecting = value;
+};
+
+socketHandler.prototype.attemptReconnecting = function() {
+	_enableReconnecting = true;
+
+	Ti.App.fireEvent("disconnected", {
+	time : new Date()).getTime() / 1000,
+	object : this
+	});
+
+};
+
+socketHandler.prototype.setCheckMessagingStatusInterval = function(value) {
+	_checkMessagingStatusInterval = value;
+};
+
 socketHandler.prototype.websocket = websocket;
-module.exports = socketHandler; 
+
+module.exports = socketHandler;
